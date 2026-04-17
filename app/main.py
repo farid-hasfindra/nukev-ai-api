518851606b6cda1ccd451b8fce1a1ef6743068c2
Farid Hasfindra <farid06hasfindra@gmail.com>
2026-04-18 04:43:04 +0700

    a

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json"
+    openapi_url=f"{settings.API_V1_STR}/openapi.json"  # For Swagger UI in Browser
 )
 
 # Set all CORS enabled origins
